c2f4b63de1f4635d6a04806f9d092d7a7a173562
ArmandleRoux98 <armand.leroux06@gmail.com>
2025-12-19 04:14:53 +0200

    Added a really nice feature to test.py and test2.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 print("A simple print.")
 print("Another print")
 print("Yet another print")
+print("This is a new feature.")
--- test2.py
+++ test2.py
@@ -1,1 +1,2 @@
 print("This is a really cool feature.")
+print("This is another new feature.")
